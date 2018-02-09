25d484b947a6177c04d073ffd31769c85590e474
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-02-09 16:51:30 +0530

    Create count space.c

---
+++ count space.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[100];
+	int i,k,count=0;
+	scanf("%[^\n]s",a);
+	for(k=0;a[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+	{
+	
+	if(a[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
