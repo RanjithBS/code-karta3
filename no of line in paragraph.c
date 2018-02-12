453a9a64baf32fd2d42bad206d7ce293cac0e029
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-02-12 16:59:51 +0530

    Create no of line in paragraph.c

---
+++ no of line in paragraph.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	
+	char ch[100];
+	int i,a,count=1;
+	gets(ch);
+	for(a=0;ch[a]!='\0';++a)
+	{
+		
+	}
+	for(i=0;i<a;i++)
+	{
+	
+	if(ch[i]=='.'||ch[i]=='?'||ch[i]==!)
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
