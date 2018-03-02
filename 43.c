74ef1f7ca3c2c9f026aa86284e30c6c7d5f05f53
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-03-02 02:53:12 -0800

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+int i;
+char a[100];
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+    if(a[i]!=' ')
+    {
+    printf("%c",a[i]);
+    }
+    
+}
+
+}
+          
